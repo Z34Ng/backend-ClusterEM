002f568e8553a2eaa728c2489e27eaaea07709c4
Z34Ng <68658172+Z34Ng@users.noreply.github.com>
2021-08-13 22:47:21 -0500

    v1.5

--- src/main/java/org/kramerlab/wekarestapi/Structure.java
+++ src/main/java/org/kramerlab/wekarestapi/Structure.java
@@ -5,7 +5,7 @@ public class Structure {
     public Object[] instancia;
     
     public Structure(Object[] vals, String n_cluster){
-        instancia = new Object[vals.length+1];
+        instancia = new Object[vals.length];
         instancia[0]=n_cluster;
         System.arraycopy(vals, 0, instancia, 1, vals.length-1); 
     }
